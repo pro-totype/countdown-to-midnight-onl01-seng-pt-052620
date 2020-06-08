ba5ece81020db3e9440dea7edf269d765f548899
Sean B <butts.sean01@gmail.com>
2020-06-08 09:18:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
 def countdown
+number= 12
+  while number > 3
+  puts "#{number} SECOND(S)!"
+  number = number -= 1
+  if number < 4
+    puts "#{number}"
+    number = number -= 1
+  if number == 0
+    puts "HAPPY NEW YEAR!"
+    break
+  end
 end
+end
+end
+countdown
